61cc3af107eeaf1264d80ebdaf12e4534bd08ae8
ThanosBb3 <el17151@mail.ntua.gr>
2021-03-14 22:26:17 +0200

    final commit

---
+++ Project/echarge/backend/testing/test_in_chrome.py
@@ -0,0 +1,32 @@
+import pytest
+#driver.get('http://www.thetestingworld.com/testings')
+from selenium import webdriver
+from selenium.webdriver import Chrome
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+#driver = webdriver.Chrome(options=options)
+
+@pytest.fixture(scope="module")
+def set_path():
+    global driver
+    #Place the path for the driver for your own machine below
+    path = "C:\\Users\\user\\Desktop\\chromedriver_win32\\chromedriver.exe"
+    driver = Chrome(executable_path=path, options=options)
+    yield
+    driver.close()
+
+def test_registration_valid_data(set_path):
+    driver.get('http://localhost:8765/evcharge/api/Login?next=%2Fevcharge%2Fapi%2F')
+    driver.maximize_window()
+    assert "Login" == driver.title
+
+def test_registration_invalid_data(set_path):
+    driver.get('http://localhost:8765/evcharge/api/Login?next=%2Fevcharge%2Fapi%2F')
+    driver.maximize_window()
+    assert driver.current_url == "http://localhost:8765/evcharge/api/Login?next=%2Fevcharge%2Fapi%2F"
+
+def test_valid_data(set_path):
+    driver.get('http://localhost:8765/evcharge/api/Login?next=%2Fevcharge%2Fapi%2F')
+    driver.maximize_window()
